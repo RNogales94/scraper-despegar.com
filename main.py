306d2f09162ad5a19ba93d962cd78341109198b7
RNogales94 <rafael.nogales@shell.com>
2023-09-16 17:47:20 +0200

    add class mongo client

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
@@ -20,13 +19,5 @@ for f in flights:
     print('=' * 40)
 
 
-
-
-
-
-
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
 driver.close()
 
